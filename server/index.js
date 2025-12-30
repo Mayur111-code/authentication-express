057309fb0a38446c20f1706c89031e0bc8060002
Mayur Borse 👨🏻‍💻 <mayurborse7440@gmail.com>
2025-12-30 17:21:40 +0530

    Add localhost to CORS allowed origins

--- server/index.js
+++ server/index.js
@@ -13,8 +13,11 @@ connectDB();
 app.use(
   cors({
     origin: [
-      "https://authentication-express-zeta.vercel.app"
+      "http://localhost:5173", 
+      "https://authentication-express-zeta.vercel.app" 
     ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
