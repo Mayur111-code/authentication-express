e1a19a04e1dd0c9706f5028865b8163e8241496d
Mayur Borse 👨🏻‍💻 <mayurborse7440@gmail.com>
2025-12-30 16:56:12 +0530

    Refactor server/index.js for improved structure

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const app =  express();
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const authRoute = require('./routes/auth.route')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoute = require("./routes/auth.route");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
+
 dotenv.config();
-connectDB()
+connectDB();
+
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // React
+    origin: [
+      "http://localhost:5173",          
+      "https://your-frontend-url.onrender.com" 
+    ],
     credentials: true,
   })
 );
@@ -18,18 +23,19 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use('/auth', authRoute);
+app.use(express.urlencoded({ extended: true }));
 
 
+app.use("/auth", authRoute);
 
-app.get('/', (req, res) => {
-    res.send('Hey form server')
+
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
 });
 
-const port = process.env.PORT
-const host = process.env.HOST
 
- app.listen(port, host, ()=>{
-    console.log(`http://${host}:${port}`)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
