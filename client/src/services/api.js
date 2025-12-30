0c02ab518edce593fbd021cbfbe8f9939bf862d5
Mayur Borse 👨🏻‍💻 <mayurborse7440@gmail.com>
2025-12-30 17:08:16 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://authentication-express-rz1w.onrender.com",
   withCredentials: true,
 });
 
